b34d28ba38830d455b6537e11b75591980984f5a
Vaishnavi Anil Ande <vaishnaviande132003@gmail.com>
2023-09-30 11:14:33 +0530

    Add files via upload

---
+++ TicTacToe.App/Main.java
@@ -0,0 +1,9 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+class Main {
+	Main() {
+	}
+
+	public static void main(String[] var0) {
+		new TicTacToe();
+	}
+}
---
+++ TicTacToe.App/TempCodeRunFile.java
@@ -0,0 +1,13 @@
+class TempCodeRunFile {
+    /**
+     * @param args
+     */
+    public static void main(String args[]) {
+        try {
+
+        } catch (Exception e) {
+
+        }
+        new TicTacToe();
+    }
+}
